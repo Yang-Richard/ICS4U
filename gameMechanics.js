cf2f4e048c12d0ca92e4010c397105d838b508f4
Richard Yang <richard.yang@hotmail.ca>
2013-12-10 11:04:11 -0500

    December 10

---
+++ gameMechanics.js
@@ -0,0 +1,101 @@
+var level = parseInt(sessionStorage.difficulty, 10);
+	//var level = 2;
+	var terms = new Array();
+	var score = 0;
+	var time=15;
+
+	function checkAnswer(){
+		var compAnswer = terms[2];
+		var userAnswer = parseInt(document.getElementById('userInput').value, 10);
+	
+		if (compAnswer == userAnswer){
+			document.getElementById("check").innerHTML = "Correct!";
+			score ++;
+			document.getElementById("score").innerHTML = "Score: " + score;
+		}
+		else{
+			document.getElementById("check").innerHTML = "Incorrect! The correct answer was " + compAnswer;
+		}
+	}
+	
+	function createQuestion(){
+		//$('#type').append(sessionStorage.difficulty);
+		var operator = createOperator(level);
+		
+		do{
+			terms = createTerms(operator);
+		} while(!isGoodAnswer(terms[2]));
+		
+		
+		switch(operator)
+		{
+			case 0:
+				terms[3] = "+";
+				break;
+			case 1:
+				terms[3] = "-";
+				break;
+			case 2:
+				terms[3] = "x";
+				break;
+			case 3:
+				terms[3] = "/";
+				break;
+		}
+		
+		document.getElementById("question").innerHTML = terms[0] + ' ' + terms[3] + ' ' + terms[1] + ' = ________';
+	
+		var myVar=setInterval(function(){myTimer()},100);
+	}
+	
+	function myTimer()
+{
+	var t = time - 0.1;
+	document.getElementById("timer").innerHTML=t;
+}
+	
+	function createOperator(difficulty){
+		
+		switch(difficulty){
+			case 0:
+				return Math.floor(Math.random() * 2);
+			case 1:
+				return Math.floor(Math.random() * 2 + 2);
+			case 2:
+				return Math.floor(Math.random() * 4);
+		}
+	}
+	
+
+	function createTerms(operator){
+		var terms = new Array();
+		switch(operator)
+		{
+			case 0:
+				terms[0] = Math.floor(Math.random() * 15);
+				terms[1] = Math.floor(Math.random() * 15);
+				terms[2] = terms[0] + terms[1];
+				return terms;
+			case 1:
+				terms[0] = Math.floor(Math.random() * 15);
+				terms[1] = Math.floor(Math.random() * 15);
+				terms[2] = terms[0] - terms[1];
+				return terms;
+			case 2:
+				terms[0] = Math.floor(Math.random() * 5);
+				terms[1] = Math.floor(Math.random() * 5);
+				terms[2] = terms[0] * terms[1];
+				return terms;
+			case 3:
+				terms[0] = Math.floor(Math.random() * 50);
+				terms[1] = Math.floor(Math.random() * 9) + 1;
+				terms[2] = terms[0] / terms[1];
+				return terms;
+		}
+	}
+	
+
+	function isGoodAnswer(answer){
+		return answer == parseInt(answer) && answer >= 0;
+	}
+	
